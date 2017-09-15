5a8b16269e12771b709be91d576bba73f1c1c21c
DrEembryo <30799788+DrEembryo@users.noreply.github.com>
2017-09-15 20:48:24 +0300

    я хз как это работает

--- KursovayaYP/Form1.Designer.cs
+++ KursovayaYP/Form1.Designer.cs
@@ -39,7 +39,7 @@
             this.lb_StudNumber.Font = new System.Drawing.Font("Consolas", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
             this.lb_StudNumber.Location = new System.Drawing.Point(12, 26);
             this.lb_StudNumber.Name = "lb_StudNumber";
-            this.lb_StudNumber.Size = new System.Drawing.Size(376, 28);
+            this.lb_StudNumber.Size = new System.Drawing.Size(578, 42);
             this.lb_StudNumber.TabIndex = 0;
             this.lb_StudNumber.Text = "Введите номер студенческого:";
             this.lb_StudNumber.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
@@ -49,7 +49,7 @@
             this.mtb_StudNumb.Location = new System.Drawing.Point(12, 80);
             this.mtb_StudNumb.Mask = "00000000000000";
             this.mtb_StudNumb.Name = "mtb_StudNumb";
-            this.mtb_StudNumb.Size = new System.Drawing.Size(376, 25);
+            this.mtb_StudNumb.Size = new System.Drawing.Size(578, 34);
             this.mtb_StudNumb.TabIndex = 1;
             this.mtb_StudNumb.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
             // 
@@ -57,7 +57,7 @@
             // 
             this.but_Login.Location = new System.Drawing.Point(158, 126);
             this.but_Login.Name = "but_Login";
-            this.but_Login.Size = new System.Drawing.Size(75, 28);
+            this.but_Login.Size = new System.Drawing.Size(288, 28);
             this.but_Login.TabIndex = 2;
             this.but_Login.Text = "Войти";
             this.but_Login.UseVisualStyleBackColor = true;
@@ -65,9 +65,9 @@
             // 
             // Form1
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 18F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(13F, 27F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(400, 190);
+            this.ClientSize = new System.Drawing.Size(611, 167);
             this.Controls.Add(this.but_Login);
             this.Controls.Add(this.mtb_StudNumb);
             this.Controls.Add(this.lb_StudNumber);
@@ -77,6 +77,7 @@
             this.MinimizeBox = false;
             this.Name = "Form1";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Load += new System.EventHandler(this.Form1_Load);
             this.ResumeLayout(false);
             this.PerformLayout();
 
--- KursovayaYP/Form1.cs
+++ KursovayaYP/Form1.cs
@@ -6,6 +6,10 @@ using System.Drawing;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+/**/
+using System.Text;
+using System.Net;
+using System.Net.Sockets;
 using System.Windows.Forms;
 
 namespace KursovayaYP
@@ -16,12 +20,71 @@ namespace KursovayaYP
         {
             InitializeComponent();
         }
+       
+        /*  try
+                    {
+                        SendMessageFromSocket(11000);
+                    }
+                    catch (Exception ex)
+                    {
+                        Console.WriteLine(ex.ToString());
+                    }
+                    finally
+                    {
+                        Console.ReadLine();
+                    }
+                
+
+                static void SendMessageFromSocket(int port)
+                {
+                    // Буфер для входящих данных
+                    byte[] bytes = new byte[1024];
+
+                    // Соединяемся с удаленным устройством
+
+                    // Устанавливаем удаленную точку для сокета
+                    IPHostEntry ipHost = Dns.GetHostEntry("localhost");
+                    IPAddress ipAddr = ipHost.AddressList[0];
+                    IPEndPoint ipEndPoint = new IPEndPoint(ipAddr, port);
+
+                    Socket sender = new Socket(ipAddr.AddressFamily, SocketType.Stream, ProtocolType.Tcp);
+
+                    // Соединяем сокет с удаленной точкой
+                    sender.Connect(ipEndPoint);
+
+                    Console.Write("Введите номер студенческого билета : ");
+                    string message = Console.ReadLine();
+
+                    byte[] msg = Encoding.UTF8.GetBytes(message);
+
+                    // Отправляем данные через сокет
+                    int bytesSent = sender.Send(msg);
+
+                    // Получаем ответ от сервера
+                    int bytesRec = sender.Receive(bytes);
+
+                    Console.WriteLine("\nОтвет от сервера: {0}\n\n", Encoding.UTF8.GetString(bytes, 0, bytesRec));
+
+                    // Используем рекурсию для неоднократного вызова SendMessageFromSocket()
+                    if (message.IndexOf(".End>") == -1)
+                        SendMessageFromSocket(port);
+
+                    // Освобождаем сокет
+                    sender.Shutdown(SocketShutdown.Both);
+                    sender.Close();*/
+             
+                  
 
         private void but_Login_Click(object sender, EventArgs e)
         {
             this.Hide();
             MainScreen screen = new MainScreen();
             screen.Show();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
         }
     }
 }
