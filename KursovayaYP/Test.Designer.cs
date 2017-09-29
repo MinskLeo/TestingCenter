25d05db4289d6dc6125d40ff4cfee451dc2bc86b
Andrei <2119930@gmail.com>
2017-09-29 13:19:37 +0300

    На половину работает загрузка вариантов ответов и самого вопроса.

--- KursovayaYP/Test.Designer.cs
+++ KursovayaYP/Test.Designer.cs
@@ -28,34 +28,148 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.lb_Question = new System.Windows.Forms.Label();
+            this.gb_Answers = new System.Windows.Forms.GroupBox();
+            this.flow_Questions = new System.Windows.Forms.FlowLayoutPanel();
+            this.cb_1 = new System.Windows.Forms.CheckBox();
+            this.cb_2 = new System.Windows.Forms.CheckBox();
+            this.cb_3 = new System.Windows.Forms.CheckBox();
+            this.cb_4 = new System.Windows.Forms.CheckBox();
+            this.cb_5 = new System.Windows.Forms.CheckBox();
+            this.cb_6 = new System.Windows.Forms.CheckBox();
+            this.gb_Answers.SuspendLayout();
             this.SuspendLayout();
             // 
-            // textBox1
+            // lb_Question
             // 
-            this.textBox1.Location = new System.Drawing.Point(12, 12);
-            this.textBox1.Multiline = true;
-            this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(476, 542);
-            this.textBox1.TabIndex = 0;
+            this.lb_Question.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.lb_Question.Location = new System.Drawing.Point(12, 9);
+            this.lb_Question.Name = "lb_Question";
+            this.lb_Question.Size = new System.Drawing.Size(1141, 145);
+            this.lb_Question.TabIndex = 0;
+            this.lb_Question.Text = "[текст вопроса]";
+            // 
+            // gb_Answers
+            // 
+            this.gb_Answers.Controls.Add(this.cb_6);
+            this.gb_Answers.Controls.Add(this.cb_5);
+            this.gb_Answers.Controls.Add(this.cb_4);
+            this.gb_Answers.Controls.Add(this.cb_3);
+            this.gb_Answers.Controls.Add(this.cb_2);
+            this.gb_Answers.Controls.Add(this.cb_1);
+            this.gb_Answers.Location = new System.Drawing.Point(12, 157);
+            this.gb_Answers.Name = "gb_Answers";
+            this.gb_Answers.Size = new System.Drawing.Size(1141, 276);
+            this.gb_Answers.TabIndex = 1;
+            this.gb_Answers.TabStop = false;
+            this.gb_Answers.Text = "Варианты ответов:";
+            // 
+            // flow_Questions
+            // 
+            this.flow_Questions.AutoScroll = true;
+            this.flow_Questions.Location = new System.Drawing.Point(12, 439);
+            this.flow_Questions.Name = "flow_Questions";
+            this.flow_Questions.Padding = new System.Windows.Forms.Padding(0, 0, 2, 0);
+            this.flow_Questions.Size = new System.Drawing.Size(1141, 54);
+            this.flow_Questions.TabIndex = 2;
+            this.flow_Questions.WrapContents = false;
+            // 
+            // cb_1
+            // 
+            this.cb_1.AutoSize = true;
+            this.cb_1.Location = new System.Drawing.Point(30, 30);
+            this.cb_1.Name = "cb_1";
+            this.cb_1.Size = new System.Drawing.Size(91, 22);
+            this.cb_1.TabIndex = 0;
+            this.cb_1.Text = "answer_1";
+            this.cb_1.UseVisualStyleBackColor = true;
+            this.cb_1.Visible = false;
+            // 
+            // cb_2
+            // 
+            this.cb_2.AutoSize = true;
+            this.cb_2.Location = new System.Drawing.Point(30, 70);
+            this.cb_2.Name = "cb_2";
+            this.cb_2.Size = new System.Drawing.Size(91, 22);
+            this.cb_2.TabIndex = 1;
+            this.cb_2.Text = "answer_2";
+            this.cb_2.UseVisualStyleBackColor = true;
+            this.cb_2.Visible = false;
+            // 
+            // cb_3
+            // 
+            this.cb_3.AutoSize = true;
+            this.cb_3.Location = new System.Drawing.Point(30, 110);
+            this.cb_3.Name = "cb_3";
+            this.cb_3.Size = new System.Drawing.Size(91, 22);
+            this.cb_3.TabIndex = 2;
+            this.cb_3.Text = "answer_3";
+            this.cb_3.UseVisualStyleBackColor = true;
+            this.cb_3.Visible = false;
+            // 
+            // cb_4
+            // 
+            this.cb_4.AutoSize = true;
+            this.cb_4.Location = new System.Drawing.Point(30, 150);
+            this.cb_4.Name = "cb_4";
+            this.cb_4.Size = new System.Drawing.Size(91, 22);
+            this.cb_4.TabIndex = 3;
+            this.cb_4.Text = "answer_4";
+            this.cb_4.UseVisualStyleBackColor = true;
+            this.cb_4.Visible = false;
+            // 
+            // cb_5
+            // 
+            this.cb_5.AutoSize = true;
+            this.cb_5.Location = new System.Drawing.Point(30, 190);
+            this.cb_5.Name = "cb_5";
+            this.cb_5.Size = new System.Drawing.Size(91, 22);
+            this.cb_5.TabIndex = 4;
+            this.cb_5.Text = "answer_5";
+            this.cb_5.UseVisualStyleBackColor = true;
+            this.cb_5.Visible = false;
+            // 
+            // cb_6
+            // 
+            this.cb_6.AutoSize = true;
+            this.cb_6.Location = new System.Drawing.Point(30, 230);
+            this.cb_6.Name = "cb_6";
+            this.cb_6.Size = new System.Drawing.Size(91, 22);
+            this.cb_6.TabIndex = 5;
+            this.cb_6.Text = "answer_6";
+            this.cb_6.UseVisualStyleBackColor = true;
+            this.cb_6.Visible = false;
             // 
             // Test
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 18F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(1018, 566);
-            this.Controls.Add(this.textBox1);
+            this.ClientSize = new System.Drawing.Size(1165, 582);
+            this.Controls.Add(this.flow_Questions);
+            this.Controls.Add(this.gb_Answers);
+            this.Controls.Add(this.lb_Question);
+            this.Font = new System.Drawing.Font("Consolas", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Margin = new System.Windows.Forms.Padding(4);
             this.Name = "Test";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "[Название предмета]-[ФИО сдающего]";
             this.Load += new System.EventHandler(this.Test_Load);
+            this.gb_Answers.ResumeLayout(false);
+            this.gb_Answers.PerformLayout();
             this.ResumeLayout(false);
-            this.PerformLayout();
 
         }
 
         #endregion
 
-        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label lb_Question;
+        private System.Windows.Forms.GroupBox gb_Answers;
+        private System.Windows.Forms.FlowLayoutPanel flow_Questions;
+        private System.Windows.Forms.CheckBox cb_6;
+        private System.Windows.Forms.CheckBox cb_5;
+        private System.Windows.Forms.CheckBox cb_4;
+        private System.Windows.Forms.CheckBox cb_3;
+        private System.Windows.Forms.CheckBox cb_2;
+        private System.Windows.Forms.CheckBox cb_1;
     }
 }
--- KursovayaYP/Test.cs
+++ KursovayaYP/Test.cs
@@ -14,7 +14,9 @@ namespace KursovayaYP
     {
         private static string ID;
         private static string[] TEST;
-        //private static List<string> TEST=new List<string>();
+        //Поля самого теста
+        private int QuestionsCount;
+        private static Question[] questions;
 
         public Test(string id,string[] test)
         {
@@ -25,10 +27,142 @@ namespace KursovayaYP
 
         private void Test_Load(object sender, EventArgs e)
         {
-            for(int i=0;i<TEST.Length;i++)//DEBUG
+            QuestionsCount = Convert.ToInt32(TEST[0]);//Кол во вопросов
+            questions = new Question[QuestionsCount];//Определяем размер массива
+            for(int i=0;i<QuestionsCount;i++)
             {
-                textBox1.Text += TEST[i];
+                flow_Questions.Controls.Add(new Button() { Text=(i+1).ToString(),AutoSize=true,AutoSizeMode=AutoSizeMode.GrowAndShrink});//DEBUG
+                flow_Questions.Controls[i].Click += new EventHandler(FlowClick);
             }
+
+            //Тут мы мутим наш массив с вопросами-----------P.S.Тут будет грязно)
+            int k = 1;//Наша прыгалка по строчкам
+            for(int i=1;i<=QuestionsCount;i++)
+            {
+                questions[i - 1] = new Question();
+                questions[i - 1].question = TEST[k];
+                k++;//Переходим на строку с кол вом ответов
+                questions[i - 1].ansCount = Convert.ToInt32(TEST[k]);
+                k++;//Переходим на строку с самими ответами
+                for(int j=0;j<questions[i-1].ansCount;j++)
+                {
+                    questions[i - 1].answers.Add(TEST[k]);
+                    k++;
+                }
+                questions[i - 1].rightAnswers = TEST[k];
+                k++;//Переходим на строку с следующим вопросом
+            }
+
+        }
+
+        private void LoadQuestion(int n)
+        {
+            lb_Question.Text = questions[n].question;
+            if(questions[n].ansCount>=1)
+            {
+                cb_1.Text = questions[n].answers[0];
+            }
+            if(questions[n].ansCount>=2)
+            {
+                cb_2.Text = questions[n].answers[1];
+            }
+            if(questions[n].ansCount>=3)
+            {
+                cb_3.Text = questions[n].answers[2];
+            }
+            if(questions[n].ansCount>=4)
+            {
+                cb_4.Text = questions[n].answers[3];
+            }
+            if(questions[n].ansCount>=5)
+            {
+                cb_5.Text = questions[n].answers[4];
+            }
+            if(questions[n].ansCount==6)
+            {
+                cb_6.Text = questions[n].answers[5];
+            }
+            switch(questions[n].ansCount)
+            {
+                case 1:
+                    cb_1.Visible = true;
+                    cb_2.Visible = false;
+                    cb_3.Visible = false;
+                    cb_4.Visible = false;
+                    cb_5.Visible = false;
+                    cb_6.Visible = false;
+                    break;
+                case 2:
+                    cb_1.Visible = true;
+                    cb_2.Visible = true;
+                    cb_3.Visible = false;
+                    cb_4.Visible = false;
+                    cb_5.Visible = false;
+                    cb_6.Visible = false;
+                    break;
+                case 3:
+                    cb_1.Visible = true;
+                    cb_2.Visible = true;
+                    cb_3.Visible = true;
+                    cb_4.Visible = false;
+                    cb_5.Visible = false;
+                    cb_6.Visible = false;
+                    break;
+                case 4:
+                    cb_1.Visible = true;
+                    cb_2.Visible = true;
+                    cb_3.Visible = true;
+                    cb_4.Visible = true;
+                    cb_5.Visible = false;
+                    cb_6.Visible = false;
+                    break;
+                case 5:
+                    cb_1.Visible = true;
+                    cb_2.Visible = true;
+                    cb_3.Visible = true;
+                    cb_4.Visible = true;
+                    cb_5.Visible = true;
+                    cb_6.Visible = false;
+                    break;
+                case 6:
+                    cb_1.Visible = true;
+                    cb_2.Visible = true;
+                    cb_3.Visible = true;
+                    cb_4.Visible = true;
+                    cb_5.Visible = true;
+                    cb_6.Visible = true;
+                    break;
+            }
+        }
+
+        private void FlowClick(object sender, EventArgs e)//Клик по кнопкам в нижней панели с вопросами
+        {
+            //MessageBox.Show("Text: "+(sender as Button).Text); //DEBUG
+            int k = Convert.ToInt32((sender as Button).Text);
+            LoadQuestion(k-1);
+            //lb_Question.Text = questions[k-1].question;
+            //gb_Answers.Controls.Clear();
+        }
+    }
+
+    class Question
+    {
+        public string question;//1 строка
+        public int ansCount;//2 строка
+        public List<string> answers = new List<string>();
+        public string rightAnswers;
+        public Question()
+        {
+
+        }
+    }
+    class Answer
+    {
+        public bool answered = false;
+        public string answer;
+        public Answer()
+        {
+
         }
     }
 }
